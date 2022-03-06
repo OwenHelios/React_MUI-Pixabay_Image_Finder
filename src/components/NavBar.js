7c985b99e33715f005bb9b3adc046e8a42d2efac
Owen Helios <owenhelios@outlook.com>
2022-03-06 10:07:41 +0800

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar'
+
+export class NavBar extends Component {
+  render() {
+    return (
+      <AppBar title="Pixabay Image Finder" />
+    )
+  }
+}
+
+
+export default NavBar
